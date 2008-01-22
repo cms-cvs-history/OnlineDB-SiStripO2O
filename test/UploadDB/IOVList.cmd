48be13731a46587e356caf03fab18a51f674a299
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2008-01-22 09:54:56 +0000

    tidy up

--- test/UploadDB/IOVList.cmd
+++ test/UploadDB/IOVList.cmd
@@ -1,6 +1,8 @@
+mkdir log
 echo 'wget -q -r "http://cmsdaq.cern.ch/cmsmon/cmsdb/servlet/RunSummaryTIF?RUN_BEGIN=1&RUN_END=1000000000&TEXT=1&DB=omds" -O log/AllRuns.txt'
 
 wget -q -r "http://cmsdaq.cern.ch/cmsmon/cmsdb/servlet/RunSummaryTIF?RUN_BEGIN=1&RUN_END=1000000000&TEXT=1&DB=omds" -O log/AllRuns.txt
 
 echo "extracting o2o runs"
+echo 
 cat log/AllRuns.txt | sort -n -k 1 | awk -F"\t" 'BEGIN{fedv=0} {if (NR>1 && ( $3 == "LATENCY" || $3 == "PHYSIC" || $3 == "PHYSIC_ZERO_SUPPRESSION" ) && fedv != $13) {fedv=$13;  a=sprintf("%d \t %30s \t  %30s \t %6.2f \t %s",$1,$2,$3,$13,$15); print a} }' | sort -n -k 1 >! log/O2ORuns.txt
--- test/UploadDB/StartIOVUpload.sh
+++ test/UploadDB/StartIOVUpload.sh
@@ -40,7 +40,7 @@ function CreateHistoTable(){
       i=1
       for file in `ls $webpath/IMG/Run*_*${identifier}*${subdet}*png 2>/dev/null`
 	do
-	htmlfile=`echo $file | sed -e "s@/data1@$webadd@"`	
+	htmlfile=`echo $file | sed -e "s@$webpathBase@$webadd@"`	
 	echo '<td valign="middle" align="center">'
 	run=`echo $file| awk -F"Run_" '{print $2}' | awk -F"_" '{print $1}'`
 	echo "<br><h4>Run $run</h4><br>"
@@ -76,7 +76,7 @@ function CreateTrendTable(){
       do
       file=${Basefile}_${det}
       #[ ! -e ${file}.eps ] && continue
-      htmlfile=`echo $file | sed -e "s@/data1@$webadd@"`	
+      htmlfile=`echo $file | sed -e "s@$webpathBase@$webadd@"`	
       echo '<td valign="middle" align="center"><h4>'$det'</h4>'
       echo '<a href="'${htmlfile}.eps'"><img src="'${htmlfile}.png'"  style="border: 0px solid ; width: '$thewidth'px; "></a>'
       if [ "$det" == "TID" ] || [ "$det" ==  "TEC" ]  ;
@@ -90,11 +90,6 @@ function CreateTrendTable(){
 
 
 function CreateHtml(){
-    export webadd="http://cmstac11.cern.ch:8080"
-    export htmlpath=`echo $webpath | sed -e "s@/data1@$webadd@"`
-
-    webfile=/data1/MonitorO2O/$1/MonitorO2O_${vTag}.html
-    webfiletmp=/tmp/$USER/SummaryO2O
 
     tableSeparator="<TD align=center>"
     tableHSeparator="<TH align=center>"
@@ -146,7 +141,35 @@ function CreateHtml(){
 #    rm -f ${webfiletmp}*
 }
 
+function perRunAnalysis(){
 
+grep TIF2 log/O2ORuns.txt | while read line
+  do
+
+  run=`echo $line | cut -f 1 -d " "`
+  partition=`echo $line | cut -f 2 -d " " `
+  Mfedv=`echo $line | cut -f 4 -d " " | cut -f 1 -d "."`
+  mfedv=`echo $line | cut -f 4 -d " " | cut -f 2 -d "."`
+
+  echo -e "\n\n\n"
+  echo .... analyzing run $run $partition $Mfedv $mfedv
+
+  cat template_sistripo2o.cfg | sed -e "s@insert_Run@$run@g" -e "s@insert_ConfigDbPartition@$partition@g" -e "s@insert_fedmajor@$Mfedv@g" -e "s@insert_fedminor@$mfedv@g" > log/SiStripO2O_$run.cfg
+
+  echo "cmsRun  log/SiStripO2O_$run.cfg > log/Log_$run.log"
+
+  cd ../MonitorO2O
+  [ ! -e $tmpbase/SiStripMonitorPedNoise_${CondDB}_${vTag}_${run}.out ] && ./SiStripMonitorPedNoise.sh -run=$run -tagP=$tagp -tagN=$tagn -tagCab=$tagc -CondDb=${CondDB} -tagIOV=${vTag}
+  
+  if [ -e $tmpbase/root/MonitorPedNoise_${CondDB}_${vTag}.root ]; then
+      echo -e "\nroot.exe -q -b -l \"RunAnalyzePedNoise.C(\"$tmpbase/root/MonitorPedNoise_${CondDB}_${vTag}.root\",\"$run\",\"$webpath/IMG\")\""
+      root.exe -q -b -l "RunAnalyzePedNoise.C(\"$tmpbase/root/MonitorPedNoise_${CondDB}_${vTag}.root\",\"$run\",\"$webpath/IMG\")" >> $tmpbase/log/RootDisplay_${CondDB}_${vTag}.txt
+      echo 
+  fi
+  cd -
+#break
+done
+}
 
 
 ############
@@ -159,17 +182,35 @@ cd $scriptDir
 export CondDB="cms_orcoff_int2r"
 [ "$1" != "" ] && CondDB=$1
 
-ver="pass4"
+ver="pass4_v1"
 [ "$2" != "" ] && ver=$2
   
-webpathBase=/data1/MonitorO2O
+#webpathBase=/data1
+webpathBase=/tmp/giordano/web
+[ "$3" != "" ] && webpathBase=$3
+
+echo CondDB $CondDB
+echo tag $ver
+echo webpathBase $webpathBase
 
 eval `scramv1 runtime -sh`
 
+#FIXME
+tagn=SiStripNoises_pass4_test_v1_n
+tagp=SiStripPedestals_pass4_test_v1_p
+tagc=SiStripCabling_pass4_test_v1
+
+
 export vTag=${ver}
-export webpath=${webpathBase}/${CondDB}/$vTag
+export webpath=${webpathBase}/MonitorO2O/${CondDB}/$vTag
 [ ! -e $webpath ] && mkdir -p $webpath && mkdir -p $webpath/IMG
 
+#export webadd="http://cmstac11.cern.ch:8080"
+export webadd="http://webcms.ba.infn.it/~giordano"
+export htmlpath=`echo $webpath | sed -e "s@${webpathBase}@$webadd@"`
+
+export webfile=$webpath/../MonitorO2O_${vTag}.html
+webfiletmp=/tmp/$USER/webfiletmp
 
 
 rm dbfile.db
@@ -181,39 +222,24 @@ mkdir $tmpbase/root
 mkdir $tmpbase/IMG
 mkdir $tmpbase/log
 
-source IOVList.cmd
+mkdir log
+echo 'wget -q -r "http://cmsdaq.cern.ch/cmsmon/cmsdb/servlet/RunSummaryTIF?RUN_BEGIN=1&RUN_END=1000000000&TEXT=1&DB=omds" -O log/AllRuns.txt'
+
+#wget -q -r "http://cmsdaq.cern.ch/cmsmon/cmsdb/servlet/RunSummaryTIF?RUN_BEGIN=1&RUN_END=1000000000&TEXT=1&DB=omds" -O log/AllRuns.txt
+
+echo "extracting o2o runs"
+echo 
+cat log/AllRuns.txt | sort -n -k 1 | awk -F"\t" 'BEGIN{fedv=0} {if (NR>1 && ( $3 == "LATENCY" || $3 == "PHYSIC" || $3 == "PHYSIC_ZERO_SUPPRESSION" ) && fedv != $13) {fedv=$13;  a=sprintf("%d \t %30s \t  %30s \t %6.2f \t %s",$1,$2,$3,$13,$15); print a} }' | sort -n -k 1 >! log/O2ORuns.txt
+
+
 rm $tmpbase/log/RootDisplay_${CondDB}_${vTag}.txt
 
-grep TIF2 log/O2ORuns.txt | while read line
-  do
-break
-  run=`echo $line | cut -f 1 -d " "`
-  partition=`echo $line | cut -f 2 -d " " `
-  Mfedv=`echo $line | cut -f 4 -d " " | cut -f 1 -d "."`
-  mfedv=`echo $line | cut -f 4 -d " " | cut -f 2 -d "."`
-
-  echo -e "\n\n\n"
-  echo .... analyzing run $run $partition $Mfedv $mfedv
-
-  cat template_sistripo2o.cfg | sed -e "s@insert_Run@$run@g" -e "s@insert_ConfigDbPartition@$partition@g" -e "s@insert_fedmajor@$Mfedv@g" -e "s@insert_fedminor@$mfedv@g" > log/SiStripO2O_$run.cfg
-
-  #cmsRun  log/SiStripO2O_$run.cfg > log/Log_$run.log
-
-  cd MonitorO2O
-  [ ! -e $tmpbase/SiStripMonitorPedNoise_${CondDB}_${vTag}_${run}.out ] && ./SiStripMonitorPedNoise.sh -run=$run -tagPN=pippo -CondDb=${CondDB} -tagIOV=${vTag}
-  
-  if [ -e $tmpbase/root/MonitorPedNoise_${CondDB}_${vTag}.root ]; then
-      echo -e "\nroot.exe -q -b -l \"RunAnalyzePedNoise.C(\"$tmpbase/root/MonitorPedNoise_${CondDB}_${vTag}.root\",\"$run\",\"$webpath/IMG\")\""
-      root.exe -q -b -l "RunAnalyzePedNoise.C(\"$tmpbase/root/MonitorPedNoise_${CondDB}_${vTag}.root\",\"$run\",\"$webpath/IMG\")" >> $tmpbase/log/RootDisplay_${CondDB}_${vTag}.txt
-      echo 
-  fi
-  cd -
-done
+perRunAnalysis
 
 echo
 echo root.exe -q -b -l "RunAnalyzePedNoise.C(\"$tmpbase/root/MonitorPedNoise_${CondDB}_${vTag}.root\",\"-1\",\"$webpath/IMG/MonitorPedNoise_${CondDB}_${vTag}\")"
 echo 
-cd MonitorO2O
+cd ../MonitorO2O
 root.exe -q -b -l "RunAnalyzePedNoise.C(\"$tmpbase/root/MonitorPedNoise_${CondDB}_${vTag}.root\",\"-1\",\"$webpath/IMG/MonitorPedNoise_${CondDB}_${vTag}\")"  > $tmpbase/log/RootDisplay_${CondDB}_${vTag}_Trend.txt
 
 cd -
@@ -226,10 +252,7 @@ CreateHtml $CondDB
 mkdir $webpath/root
 cp $tmpbase/root/MonitorPedNoise_${CondDB}_${vTag}.root $webpath/root
 cp log/O2ORuns.txt $webpath
-
-
-
-
+echo webfile $webfile
 
 
 
